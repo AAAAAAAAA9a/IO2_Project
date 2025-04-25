6c652dbe0c067e375ba71e08c858a282eb7d8561
AAAAAAAAA9a <lisowski5t@gmail.com>
2025-04-25 05:22:59 +0200

    Refactor application structure and add configuration management; enhance model documentation and form validation

--- file_functions.py
+++ file_functions.py
@@ -1,7 +1,9 @@
+# Funkcja do formatowania rozmiaru pliku na czytelną postać (np. KB, MB, GB)
 def format_size(size_mb):
     size = size_mb * 1024 * 1024 
     units = ['B', 'KB', 'MB', 'GB', 'TB', 'PB']
     unit_index = 0
+    # Przeliczanie rozmiaru na odpowiednią jednostkę
     while size >= 1024 and unit_index < len(units) - 1:
         size /= 1024
         unit_index += 1
